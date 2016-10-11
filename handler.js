90d448baa7397641677e58f58b4c69545ce09876
Philipp Muens <raute1337@gmx.de>
2016-10-11 14:04:00 -0700

    Create handler.js

---
+++ handler.js
@@ -0,0 +1,28 @@
+'use strict';
+
+const AWS = require('aws-sdk');
+
+module.exports.firstLambda = (event, context, callback) => {
+  const sns = new AWS.SNS();
+
+  const accountId = '<account-id>';
+
+  const params = {
+    Message: 'triggering other Lambda(s)',
+    TopicArn: `arn:aws:sns:us-east-1:${accountId}:dispatcher`
+  };
+
+  sns.publish(params, (error, data) => {
+    if (error) {
+      callback(error);
+    }
+    callback(null, { message: 'Message successfully published to SNS topic "dispatcher"', event });
+  });
+};
+
+module.exports.secondLambda = (event, context, callback) => {
+  // print out the event information on the console (so that we can see it in the CloudWatch logs)
+  console.log(`I'm triggered by firstLambda through the SNS topic "dispatcher":\n${JSON.stringify(event, null, 2)}`);
+
+  callback(null, { event });
+};
